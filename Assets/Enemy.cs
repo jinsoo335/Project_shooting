7413bffd5ab08a2a0d3568496df47b506050eb99
DESKTOP-414MKLB\jinso <jinsoo335@naver.com>
2021-08-26 14:56:14 +0900

    Add Player Tracking

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,39 +2,57 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private float health = 25.0f;
-    public float enemy_speed = 0.015f;
-    public int enemyExp = 1;
+    public float health;
+    public float damage;
+    public float speed;
+    public float fieldOfVision;
+    public float atkRange;
+    protected Transform target = null;
+    public Transform playerPos;
+    Collider2D[] cols;
+
+    private void SetEnemyStatus(float _health, float _damage, float _speed, float _atkRange, float _fieldOfVision)
+    {
+        health = _health;
+        damage = _damage;
+        speed = _speed;
+        atkRange = _atkRange;
+        fieldOfVision = _fieldOfVision;
+    }
 
     void Update()
     {
-        
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        cols = Physics2D.OverlapCircleAll(playerPos.position, 10f);
+        //Move();
     }
 
     void Die()
     {
-        Player player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-        player.levelUp(enemyExp);
         Destroy(gameObject);
     }
 
     public void TakeDamage(float damgae)
     {
-        Debug.Log("Hit!");
         health -= damgae;
 
-        if(health <= 0)
+        if (health <= 0)
         {
             Die();
         }
     }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag.Equals("bullet"))
+        if (collision.gameObject.CompareTag("bullet"))
         {
-            TakeDamage(10f);
+            //TakeDamage(10);
+            collision.gameObject.SetActive(false);
         }
     }
+    public virtual void Move()
+    {
+
+    }
+
 
 }
---
+++ Assets/EnemyAi.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAi : MonoBehaviour
+{
+    public Transform target;
+    float attackDelay;
+
+    Enemy enemy;
+
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+    }
+
+    void Update()
+    {
+        attackDelay -= Time.deltaTime;
+        if (attackDelay < 0) attackDelay = 0;
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        float distance = Vector3.Distance(transform.position, target.position);
+        Debug.Log("update");
+        if (attackDelay == 0 && distance <= enemy.fieldOfVision)
+        {
+            Debug.Log("asdad");
+            MoveToTarget();
+        }
+
+    }
+    void MoveToTarget()
+    {
+        transform.Translate(new Vector2(target.position.x - transform.position.x, target.position.y - transform.position.y) * enemy.speed * Time.deltaTime);
+
+    }
+
+
+}
